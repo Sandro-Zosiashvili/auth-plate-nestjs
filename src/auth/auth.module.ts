07d867fd973c988a595c3292115783f2eeca30c6
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2026-03-10 02:09:47 +0400

    Feat: jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,21 @@ import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
-import { JwtService } from '@nestjs/jwt';
-import { UserRepository } from '../user/user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { User } from '../user/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, PassportModule],
-  providers: [AuthService, UserRepository],
+  imports: [
+    UserModule,
+    PassportModule,
+    TypeOrmModule.forFeature([User]), // ✅ ეს დაამატე
+    JwtModule.register({
+      secret: process.env.JWT_SECRET ?? '<JWT_SECRET>',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
